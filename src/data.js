6c9abaed9e91bbd9a2ff7ea0ab7b1626a7f47d07
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-10-14 13:09:46 +0530

    Built Shopping Categories.

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ export const sliderItems = [
         id: 2,
         img: "https://i.ibb.co/DG69bQ4/2.png",
         title: "AUTUMN COLLECTION",
-        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30%OFF FOR NEW ARRIVALS.",
         bg: "fcf1ed",
     },
     {
